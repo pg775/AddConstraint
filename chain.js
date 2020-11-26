b4ca6c6784df3592f43a934e3adcd03ef5929c7f
pg775 <67814029+pg775@users.noreply.github.com>
2020-11-25 17:52:48 -0800

    Added a constraint

---
+++ chain.js
@@ -0,0 +1,21 @@
+class Chain{
+    constructor(b1,b2){
+    var options = {
+        bodyA:b1,
+        bodyB:b2,
+        stiffness:0.04,
+        length:10,
+
+    }
+    this.chain=Constraint.create(options)
+    World.add(world, this.chain);
+    
+    }
+
+display(){
+    var pointA=this.chain.bodyA.position
+    var pointB=this.chain.bodyB.position
+    strokeWeight(4)
+    line(pointA.x,pointA.y,pointB.x,pointB.y)
+}
+}
--- sketch.js
+++ sketch.js
@@ -1,11 +1,12 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
-
+var log,chain
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
 }
@@ -36,14 +37,18 @@ function setup(){
 
     bird = new Bird(100,100);
 
+    log = new Log(100,200,150, -PI/2)
+    chain = new Chain(bird.body,log.body)
+
+
 }
 
 function draw(){
     background(backgroundImg);
     Engine.update(engine);
-    console.log(box2.body.position.x);
-    console.log(box2.body.position.y);
-    console.log(box2.body.angle);
+    //console.log(box2.body.position.x);
+    //console.log(box2.body.position.y);
+    //console.log(box2.body.angle);
     box1.display();
     box2.display();
     ground.display();
@@ -61,4 +66,10 @@ function draw(){
 
     bird.display();
     platform.display();
+
+    log.display();
+    chain.display();
+
+
+
 }
